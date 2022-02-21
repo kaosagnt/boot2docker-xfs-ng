3527bcfb024556917186ced05465677d0e807de3
Ian McWilliam <kaosagnt@gmail.com>
2022-02-21 12:38:19 +1100

    update.sh -> TCL 13.0 -> kernel 5.15

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 set -Eeuo pipefail
 
 # TODO http://distro.ibiblio.org/tinycorelinux/latest-x86_64
-major='12.x'
-version='12.0' # TODO auto-detect latest
+major='13.x'
+version='13.0' # TODO auto-detect latest
 # 9.x doesn't seem to use ".../archive/X.Y.Z/..." in the same way as 8.x :(
 
 mirrors=(
@@ -12,7 +12,7 @@ mirrors=(
 )
 
 # https://www.kernel.org/
-kernelBase='5.10'
+kernelBase='5.15'
 
 # avoid issues with slow Git HTTP interactions (*cough* sourceforge *cough*)
 export GIT_HTTP_LOW_SPEED_LIMIT='100'
